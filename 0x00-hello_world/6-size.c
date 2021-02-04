532b0c6be15c066b8a021e6df1a4c924b31c063e
Felipe Hernandez <fhc0@yahoo.com>
2021-02-04 18:37:16 +0000

    Ok

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ float tipoFlotante;
 double tipoDoble;
 char tipoCaracter;
 printf("Size of int: %zu bytes\n", sizeof(tipoEntero));
-printf("Size of float: %zu bytes\n", sizeof(tipoFlotente));
+printf("Size of float: %zu bytes\n", sizeof(tipoFlotante));
 printf("Size of double: %zu bytes\n", sizeof(tipoDoble));
 printf("Size of char: %zu byte\n", sizeof(tipoCaracter));
 return (0);
