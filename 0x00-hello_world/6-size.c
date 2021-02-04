4731f1ed1c573444452cd2800f2d8aa42eba68ed
Felipe Hernandez <fhc0@yahoo.com>
2021-02-04 19:48:40 +0000

    Ok

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int tipoEnteroMedio;
 short int tipoEnteroCorto;
 long int tipoEnteroLargo;
 float tipoFlotante;
-printf("Size of a char: %d bytes(s)\n", sizeof(caracter));
-printf("Size of an int: %d bytes(s)\n", sizeof(tipoEnteroMedio));
-printf("Size of a long int: %d bytes(s)\n", sizeof(tipoEnteroCorto));
-printf("Size of a long long int: %d byte(s)\n", sizeof(tipoEnteroLargo));
-printf("Size of a float: %d byte(s)\n", sizeof(tipoFlotante));
+printf("Size of a char: %zu bytes(s)\n", sizeof(caracter));
+printf("Size of an int: %zu bytes(s)\n", sizeof(tipoEnteroMedio));
+printf("Size of a long int: %zu bytes(s)\n", sizeof(tipoEnteroCorto));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(tipoEnteroLargo));
+printf("Size of a float: %zu byte(s)\n", sizeof(tipoFlotante));
 return (0);
 }
