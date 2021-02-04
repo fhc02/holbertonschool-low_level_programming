31742190631544d3b772de4c1f34507c14b57cd5
Felipe Hernandez <fhc0@yahoo.com>
2021-02-04 15:56:25 +0000

    First Thing in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main (void){
- 
- char message="\"Programming is like building a multilingual puzzle\n"
- puts(message);
-
- return 0;
+char[50] message = "\"Programming is like building a multilingual puzzle\n"
+puts (message);
+return (0);
 }
