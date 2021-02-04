d5744b11284440745ae97703d1e57c5bdf0f7b5e
Felipe Hernandez <fhc0@yahoo.com>
2021-02-04 18:34:24 +0000

    Ok

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0
+**/
+int main(void)
+{
+int tipoEntero;
+float tipoFlotante;
+double tipoDoble;
+char tipoCaracter;
+printf("Size of int: %zu bytes\n", sizeof(tipoEntero));
+printf("Size of float: %zu bytes\n", sizeof(tipoFlotente));
+printf("Size of double: %zu bytes\n", sizeof(tipoDoble));
+printf("Size of char: %zu byte\n", sizeof(tipoCaracter));
+return (0);
+}
