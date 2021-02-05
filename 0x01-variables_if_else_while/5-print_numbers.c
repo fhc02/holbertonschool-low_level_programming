e0241d570c388165dc5a750edd95ba13c5efe3b4
Felipe Hernandez <fhc0@yahoo.com>
2021-02-05 20:55:08 +0000

    Ok-08

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0 (Success)
+**/
+int main(void)
+{
+	short num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d", num);
+	putchar('\n');
+	return (0);
+}
