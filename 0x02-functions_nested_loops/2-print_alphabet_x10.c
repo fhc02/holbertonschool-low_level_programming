b135074729e111176fd5b5649ff5803054692900
Felipe Hernandez <fhc0@yahoo.com>
2021-02-10 03:21:39 +0000

    Ok

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	char lowerCase;
 	short num;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num < 10; num++)
 	{
 		for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
 		{
